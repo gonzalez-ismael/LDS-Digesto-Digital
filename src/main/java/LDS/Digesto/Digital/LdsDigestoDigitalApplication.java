02e43e317a93192e13f59cb8b75cf6c4761938d3
IsmaQty <ismaqty33@gmail.com>
2023-10-26 18:34:05 -0300

    feat(core): Configuración inicial del espacio de trabajo en Apache NetBeans con dependencias agregadas
    Se ha creado el espacio de trabajo en Apache NetBeans y se ha completado la compilación con las siguientes dependencias:
    
    - Spring Boot Dev Tools
    - Lombok
    - Spring Web
    - Spring Session (opcional, pero se incluyó)
    - MySQL Driver
    - PostgreSQL Driver
    - Spring Data JPA
    
    Todavía no se ha incorporado la arquitectura del proyecto.

---
+++ src/main/java/LDS/Digesto/Digital/LdsDigestoDigitalApplication.java
@@ -0,0 +1,13 @@
+package LDS.Digesto.Digital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdsDigestoDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdsDigestoDigitalApplication.class, args);
+	}
+
+}
